6252c45375060ee58cebec89c6df6b1663c5299d
María <frm@fernandowirtz.com>
2022-12-01 16:42:57 +0100

    Tarea02.1 enunciado

---
+++ src/es/teis/ud2/Main.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package es.teis.ud2;
+
+import es.teis.ud2.model.AccountMovement;
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author maria
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        AccountMovement accMovement = transferirDineroEntreEmpleados(7369, 7499, new BigDecimal(1500));
+        if (accMovement != null) {
+            System.out.println("Se ha creado el registro: " + accMovement);
+        }
+    }
+
+
+    private static AccountMovement transferirDineroEntreEmpleados(int empnoOrigen, int empnoDestino, BigDecimal cantidad) {
+        AccountMovement accMovement = null;
+        //Completa para crear el servicio  y llamar a su método  transferir(int empnoOrigen, int empnoDestino, BigDecimal cantidad)
+        
+        return accMovement;
+    }
+
+}
